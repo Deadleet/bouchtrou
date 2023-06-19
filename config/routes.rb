b981459b29a0d1d544f073cacae4e9b7c7dd12a5
thomas guerin <thomasguerin1313@outlook.com>
2023-06-19 21:30:27 +0200

    creation controller bouchtroue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'bouchtroues/index'
+  get 'bouchtroues/show'
+  get 'bouchtroues/new'
+  get 'bouchtroues/edit'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bouchtroues
+
 end
