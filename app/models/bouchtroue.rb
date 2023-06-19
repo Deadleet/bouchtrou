bf774d6b3e91cecac8b3c3765f5f3cea0a3d8231
thomas guerin <thomasguerin1313@outlook.com>
2023-06-19 18:36:03 +0200

    validations du model bouchtroue

--- app/models/bouchtroue.rb
+++ app/models/bouchtroue.rb
@@ -1,2 +1,11 @@
 class Bouchtroue < ApplicationRecord
+  validates :name, presence: true
+  validates :specialty, presence: true
+  validates :description, presence: true
+  # validates :rating, presence: true
+  validates :status, presence: true, default: false
+  validates :location, presence: true
+  validates :hour_price, presence: true
+  validates :available_start, presence: true
+  validates :available_end, presence: true
 end
