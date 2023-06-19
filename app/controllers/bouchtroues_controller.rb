17f358bc1dcfec6d12c2507a34fc046a69599c97
thomas guerin <thomasguerin1313@outlook.com>
2023-06-19 22:16:16 +0200

    controller bouchtroue ok

--- app/controllers/bouchtroues_controller.rb
+++ app/controllers/bouchtroues_controller.rb
@@ -1,13 +1,41 @@
 class BouchtrouesController < ApplicationController
+  before_action :set_bouchtroue, only: [:show, :edit, :update, :delete]
+
   def index
+    @bouchtroues = Bouchtroue.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @bouchtroue = Bouchtroue.new
   end
 
-  def edit
+  def create
+    @bouchtroue = Bouchtroue.new(bouchtroue_params)
+    @bouchtroue.save!
+    redirect_to bouchtroue_path(@bouchtroue)
+  end
+
+  def edit; end
+
+  def update
+    @bouchtroue.update!(bouchtroue_params)
+    redirect_to bouchtroue_path(@bouchtroue)
+  end
+
+  def destroy
+    @bouchtroue.destroy
+    redirect_to bouchtroues_path, status: :see_other
+  end
+
+  private
+
+  def set_bouchtroue
+    @bouchtroue = Bouchtroue.find(params[:id])
+  end
+
+  def bouchtroue_params
+    params.require(:bouchtroue).permit(:name, :address, :rating)
   end
 end
