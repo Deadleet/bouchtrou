6b370da59d038e91582d7d5ccd396d10b17e19ce
thomas guerin <thomasguerin1313@outlook.com>
2023-06-19 18:37:40 +0200

    migration model bouchtroue

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_134824) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_161246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bouchtroues", force: :cascade do |t|
+    t.string "name_profile"
+    t.string "specialty"
+    t.string "description"
+    t.string "location"
+    t.integer "rating"
+    t.string "status"
+    t.float "hour_price"
+    t.date "available_start"
+    t.date "available_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
