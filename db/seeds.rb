695407f17ea2064b920eac4dd3471c7588368d6c
thomas guerin <thomasguerin1313@outlook.com>
2023-06-20 04:53:38 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Bouchtroue.destroy_all
+User.destroy_all
+puts "destroy ok"
+puts "1"
+User.create(email: "jr@gmail.com", password:"123456789")
+puts "2"
+Bouchtroue.create(name_profile: "julien", specialty: "sport", description: "sport de montagne", status: "libre", location: "marseille",user: User.last)
+Bouchtroue.create(name_profile: "emeline", specialty: "bronzer", description: "jambe et bras plage", status: "libre", location: "lille",user: User.last)
+Bouchtroue.create(name_profile: "thomas", specialty: "sport", description: "sport de rue t'es vu", status: "occupé", location: "chicago",user: User.last)
+puts "3"
